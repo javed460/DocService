80ebef5565279cf3edc9f0bf0f5ad31dec38b396
Javed Bagwan <javed460@gmail.com>
2026-02-10 10:12:34 +0530

    added InvalidFileFormatException

---
+++ src/main/java/com/example/docservice/exception/InvalidFileFormatException.java
@@ -0,0 +1,9 @@
+package com.example.docservice.exception;
+
+public class InvalidFileFormatException extends RuntimeException {
+    
+    public InvalidFileFormatException(String message) {
+        super(message);
+    }
+
+}
